4d5b43be33444174f59922598f7ac16274a3f824
italo pessan <italopessan@gmail.com>
2023-12-29 09:58:18 -0300

    consulta com linq e sql puro

--- Stored_Procedure/Stored_Procedure/Controllers/ClienteController.cs
+++ Stored_Procedure/Stored_Procedure/Controllers/ClienteController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Stored_Procedure.Context;
+using System.Linq;
 
 namespace Consultas_SQL.Controllers
 {
@@ -48,15 +49,47 @@ namespace Consultas_SQL.Controllers
         [HttpGet("/ConsultaWherePorParametro")]
         public IActionResult ConsultaWhereParametro(int id)
         {
-            var clientes = _context.Pessoas.FromSql($"Select * from pessoas where id = {id}");
-            return Ok(clientes);
+            var cliente = _context.Pessoas.FromSql($"Select * from pessoas where id = {id}");
+            return Ok(cliente);
         }
 
         [HttpGet("/pesquisarPorNome")]
         public IActionResult pesquisarPorNome(string nome)
         {
-            var clientes = _context.Pessoas.FromSql($"Select * from pessoas where Nome = {nome}");
-            return Ok(clientes);
+            var cliente = _context.Pessoas.FromSql($"Select * from pessoas where Nome = {nome}");
+            return Ok(cliente);
         }
+
+        [HttpGet("/pesquisaNome")]
+        public IActionResult pesquisaNome(string nome)
+        {
+            var cliente = _context.Pessoas.Where(c => c.Nome == nome);
+            return Ok(cliente);
+        }
+
+        [HttpGet("/contains")]
+        public IActionResult contains(string cpf)
+        {
+            var filtrado = _context.Pessoas.Where(str => str.Cpf.Contains(cpf)).ToList();
+            return Ok(filtrado);
+        }
+
+        [HttpGet("/intervalo")]
+        public IActionResult intervalo(DateTime dataInicio, DateTime dataFim)
+        {
+            //1987-04-28
+            var filtro = _context.Pessoas.FromSql($"Select * from pessoas where DataNascimento BETWEEN {dataInicio} and {dataFim}");
+            return Ok(filtro);
+        }
+
+        [HttpGet("/intervaloLinq")]
+        public IActionResult intervaloLinq(DateTime dataInicio, DateTime dataFim)
+        {
+            //1987-04-28
+            var filtrado = _context.Pessoas.Where(str => str.DataNascimento >= dataInicio && str.DataNascimento <= dataFim).ToList();
+            return Ok(filtrado);
+        }
+
+
     }
 }
